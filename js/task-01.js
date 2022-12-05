3d0cab94669b7abe25d804d3c2e72a7e15785be8
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-05 18:52:14 +0100

    task-01 correction

--- js/task-01.js
+++ js/task-01.js
@@ -5,6 +5,6 @@ const listItems = document.querySelectorAll("#categories > li.item");
 console.log(`Number of categories: ${listItems.length}`);
 
 listItems.forEach((el) => {
-  console.log(`Category: ${el.querySelector("h2").textContent}`);
-  console.log(`Elements: ${el.querySelectorAll("li").length}`);
+  console.log(`Category: ${el.querySelector("h2").textContent}
+Elements: ${el.querySelectorAll("li").length}`);
 });
