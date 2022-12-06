d08b59d1ba5068ac69576ec60f04ffb9acb7140e
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-06 21:48:57 +0100

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+"use strict";
 
+const form = document.querySelector("form.login-form");
+const loginData = {};
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled!");
+  }
+  loginData.email = email.value;
+  loginData.password = password.value;
+  console.log(loginData);
+  event.currentTarget.reset();
+}
