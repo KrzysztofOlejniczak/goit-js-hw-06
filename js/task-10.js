37a9a3394e3cc068936a8f122bce7826a9b64771
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-06 23:00:43 +0100

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+"use strict";
+
+const input = document.querySelector("input");
+const boxContainer = document.querySelector("div#boxes");
+const createBtn = document.querySelector("button[data-create");
+const destroyBtn = document.querySelector("button[data-destroy");
+
+createBtn.addEventListener("click", (event) =>
+  createBoxes(parseInt(input.value))
+);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(number) {
+  for (let i = 1; i <= number; i++) {
+    const color = getRandomHexColor();
+    const box = `<div style="width:${30 + 10 * i}px; height:${
+      30 + 10 * i
+    }px; background-color:${color}"></div>`;
+    boxContainer.insertAdjacentHTML("beforeend", box);
+  }
+}
+
+function destroyBoxes() {
+  boxContainer.innerHTML = "";
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
