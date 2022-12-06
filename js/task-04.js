cfb0efec7235a301026a7a141cf4afd34bd7c736
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-06 19:22:56 +0100

    task-04 update

--- js/task-04.js
+++ js/task-04.js
@@ -5,16 +5,19 @@ const counter = document.querySelector("#value");
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 
+const updateCounter = () => {
+  console.log(counterValue);
+  counter.innerHTML = counterValue;
+};
+
 decrementBtn.addEventListener("click", () => {
   console.log("Decrement button was clicked");
   counterValue--;
-  console.log(counterValue);
-  counter.innerHTML = counterValue;
+  updateCounter();
 });
 
 incrementBtn.addEventListener("click", () => {
   console.log("Increment button was clicked");
   counterValue++;
-  console.log(counterValue);
-  counter.innerHTML = counterValue;
+  updateCounter();
 });
