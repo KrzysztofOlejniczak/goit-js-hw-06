837fa99dfb6b9b3ceb18885c41bf53d97356a6c9
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-07 18:17:05 +0100

    correction in task 7 & 10

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,8 @@
 
 const inputSlider = document.querySelector("#font-size-control");
 const textExample = document.querySelector("#text");
+
+inputSlider.value = 16;
 inputSlider.addEventListener("input", (event) => {
   console.log(event.currentTarget.value);
   textExample.style.fontSize = `${event.currentTarget.value}px`;
--- js/task-10.js
+++ js/task-10.js
@@ -4,25 +4,27 @@ const input = document.querySelector("input");
 const boxContainer = document.querySelector("div#boxes");
 const createBtn = document.querySelector("button[data-create");
 const destroyBtn = document.querySelector("button[data-destroy");
+const firstBoxSize = 30;
+let boxSize = firstBoxSize;
 
 createBtn.addEventListener("click", (event) => {
-  destroyBoxes();
   createBoxes(parseInt(input.value));
 });
+
 destroyBtn.addEventListener("click", destroyBoxes);
 
 function createBoxes(number) {
   for (let i = 1; i <= number; i++) {
     const color = getRandomHexColor();
-    const box = `<div style="width:${30 + 10 * i}px; height:${
-      30 + 10 * i
-    }px; background-color:${color}; margin: 3px 0"></div>`;
+    const box = `<div style="width:${boxSize}px; height:${boxSize}px; background-color:${color}; margin: 3px 0"></div>`;
+    boxSize += 10;
     boxContainer.insertAdjacentHTML("beforeend", box);
   }
 }
 
 function destroyBoxes() {
   boxContainer.innerHTML = "";
+  boxSize = firstBoxSize;
 }
 
 function getRandomHexColor() {
